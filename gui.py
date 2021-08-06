289933f568531a744881acb47734abedb81036d4
krgainer <krgainer@gmail.com>
2021-08-06 23:50:23 +0200

    the whole systems fucked
    merged main and gui, using gui as a temp doc.
    
    Got tkinter to display stuff sometimes, but only if the moon is in plutos shadow while a guy named jared jerks off, idk why

--- gui.py
+++ gui.py
@@ -1,8 +1,45 @@
-from kivy.app import App
-from kivy.uix.button import Button
+import tkinter as tk
+from tkinter import *
+import random
+import string
 
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + num + symbols
 
-TestApp().run()
+master = Tk()
+w1 = Scale(master, from_=4, to=32, orient=HORIZONTAL)
+w1.get()
+w1.set(12)
+w1.pack()
+w2 = Button(master, text='Show').pack()
+#w2 = Button(master, text='Show', command=sliderOutput).pack()
+
+
+sliderOutput = w1.get()
+length = int(sliderOutput) 
+strTemp = random.sample(all,length)
+password = ("".join(strTemp))
+printpass = print(password)
+
+w2 = Button(master, text='Show', command=printpass).pack()
+
+msg = tk.Entry(master, text = password)
+msg.config(bg='darkgrey', font=('times', 24, 'italic'))
+msg.pack()
+
+
+##GUI
+# master = Tk()
+# w1 = Scale(master, from_=4, to=32, orient=HORIZONTAL)
+# w1.get()
+# Button(master, text='Show', command=sliderOutput).pack()
+# w1.pack()
+# w1.set(12) 
+
+
+mainloop()
+
+
