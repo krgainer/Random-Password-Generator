e0e6e0107db903c105cafa857bed69ef64e20aec
krgainer <krgainer@gmail.com>
2021-08-07 00:20:28 +0200

    Update newmain.py
    slider now works, need to impliment text bos

--- newmain.py
+++ newmain.py
@@ -12,16 +12,17 @@ all = lower + upper + num + symbols
 
 master = Tk()
 w1 = Scale(master, from_=4, to=32, orient=HORIZONTAL)
-w1.get()
 w1.set(12)
 w1.pack()
-# w2 = Button(master, text='Show').pack()
-# w2 = Button(master, text='Show', command=sliderOutput).pack()
 
-sliderOutput = w1.get()
-length = int(sliderOutput) 
+
+
+
 def generate():
 	global password, printpass
+	w1.get()
+	sliderOutput = w1.get()
+	length = int(sliderOutput) 
 	strTemp = random.sample(all,length)
 	password = ("".join(strTemp))
 	printpass = print(password)
@@ -34,16 +35,6 @@ w2 = Button(master, text='Show', command=generate).pack()
 msg = tk.Entry(master, text = password)
 msg.config(bg='darkgrey', font=('times', 24, 'italic'))
 msg.pack()
-
-
-##GUI
-# master = Tk()
-# w1 = Scale(master, from_=4, to=32, orient=HORIZONTAL)
-# w1.get()
-# Button(master, text='Show', command=sliderOutput).pack()
-# w1.pack()
-# w1.set(12) 
-
 
 mainloop()
 
