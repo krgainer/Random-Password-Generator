faeab42fb6c2c62dda4246320800c4f765d5f662
krgainer <krgainer@gmail.com>
2021-08-06 20:00:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import random
+import string
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+length = int(input('\nPlease specify number of characters')) 
+
+strTemp = random.sample(all,length)
+
+password = ("".join(strTemp))
+
+print(password)
